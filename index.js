d9d5c65695e3fab0a66181e8f0dabb97b53b676b
Brayanfdev <brayanfreitas580@gmail.com>
2026-02-13 22:16:20 -0300

    atualização do desafio

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ class hero{
         this.tipohero = tipohero
         this.ataqueHero = ataqueHero
 
+    let poder = "magia"
+        console.log("O " + this.nameHero + " atacou usando " + poder)
     }
 
     texto(){
@@ -13,29 +15,11 @@ class hero{
         console.log(`O ${this.tipohero} ${this.nameHero} atacou o guerreiro hogbat usando ${this.ataqueHero}`)
     }
 
+   
+        
+
 }
 
 let informacoes = new hero ("devorador", "29 anos", "mago", "magia")
 
 informacoes.texto()
-
-class heroinimigo{
-    constructor(nameHeroi, idadeHeroi, tipoheroi, ataqueHeroi){
-        this.nameHeroi = nameHeroi
-        this.idadeHeroi = idadeHeroi
-        this.tipoheroi = tipoheroi
-        this.ataqueHeroi = ataqueHeroi
-
-
-    }
-
-    textos(){
-        console.log(`O nickname do jogador é ${this.nameHeroi} ele tem ${this.idadeHeroi} e é do tipo ${this.tipoheroi}`)
-        console.log(`O ${this.tipoheroi} ${this.nameHeroi} contra-atacou o mago devorador usando um poderoso golpe de ${this.ataqueHeroi}`)
-    }
-
-}
-
-let informacoes1 = new heroinimigo ("hogbat", "21 anos", "guerreiro", "espada")
-
-informacoes1.textos()
